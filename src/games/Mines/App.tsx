3922433ece37d628167e8282f612b1e177cd8d5d
NINJATSS <143434164+NINJATSS@users.noreply.github.com>
2023-09-24 15:38:55 +0100

    Update App.tsx

--- src/games/Mines/App.tsx
+++ src/games/Mines/App.tsx
@@ -78,7 +78,7 @@ function Mines() {
     try {
       await gamba.play({
         bet,
-        wager: config.wager + totalGain,
+        wager: config.wager + totalGain, creatorFee: 0.02
       })
 
       sounds.tick.play({ playbackRate: 1.5 })
